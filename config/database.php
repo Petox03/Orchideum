9b3604786712f208f070fd61df335c75aefc75d8
Alberto Sosa <petox.somart@outlook.com>
2021-02-03 14:44:14 -0500

    cambios de index

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $capsule = new Capsule;
 $capsule->addConnection([
     'driver'    => 'mysql',
     'host'      => 'localhost',
-    'database'  => '',
+    'database'  => 'orchideum',
     'username'  => 'root',
     'password'  => '',
     'charset'   => 'utf8',
